027a1ed8146baf3fb91063e6e229400fbd6f06d3
PJK <PJK@gmail.com>
2023-03-13 14:58:57 +0700

    tryTernary

---
+++ tryIfElse.js
@@ -0,0 +1,14 @@
+// const password = 'password';
+
+// if(password.length >= 12) {
+//     console.log('password is very strong so WELCOME!!!');
+// }  else if (password.length >= 8){
+//     console;log('WELCOME!!!');
+// }  else {
+//     console.log('SORRY!!! the password is too short');
+// }
+
+const password = "password12345";
+password.length >= 12 ? console.log('password is very strong so WELCOME!!!')
+:password.length >= 8 ? console.log('WELCOME!!!')
+:console.log('SORRY!!! try again');
